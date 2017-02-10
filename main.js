2e747b1a31ab61df41634535a673f5bba54c2b61
maksigit <maksi123p@gmail.com>
2017-02-10 20:55:20 +0200

    Ready

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-
-    // $('.menu-icon').on('click', function () {
-    //     $('bar-1').toggle();
-    // });
-
-
-// ===============================PROBA=================================
+// ===================================================================
 $(document).ready(function () {
 
     $('.gamburger').click(function () {
         $('.bar .lists').slideToggle(500)
+        $('.fa-bars').toggleClass('fa-sort-desc');
+
     });
     $(window).resize(function () {
         if ($(window).width()>500){
@@ -19,17 +15,17 @@ $(document).ready(function () {
 
 
 // ================================================================
-$('.sulka_1').on('click', function () {
+$('.item-up_1').on('click', function () {
     $('.item-up_1').toggleClass('fa-sort-desc');
     $('.item-up_1').toggleClass('backblue');
     $('.item-bottom_1').slideToggle('nety');
 });
-$('.sulka_2').on('click', function () {
+$('.item-up_2').on('click', function () {
     $('.item-up_2').toggleClass('fa-sort-desc');
     $('.item-up_2').toggleClass('backblue');
     $('.item-bottom_2').slideToggle('nety');
 });
-$('.sulka_3').on('click', function () {
+$('.item-up_3').on('click', function () {
     $('.item-up_3').toggleClass('fa-sort-desc');
     $('.item-up_3').toggleClass('backblue');
     $('.item-bottom_3').slideToggle('nety');
@@ -37,22 +33,20 @@ $('.sulka_3').on('click', function () {
 
 // ================================================================
 
-$('.great, .clouse a').on('click', function () {
+$('.great, .clouse').on('click', function () {
     $('.okno-pop-up').slideToggle('nety');
     $('.blackish').toggleClass('nety');
 });
 
 // ================================================================
 
-$('.third_1').on('click', function () {
-    $('.third-our_1').toggleClass('nety');
-});
-$('.third_2').on('click', function () {
-    $('.third-our_2').toggleClass('nety');
-});
-$('.third_3').on('click', function () {
-    $('.third-our_3').toggleClass('nety');
-});
+    $('.tab_item').not(":first").hide();
+    $('.tab-backg').removeClass('tab-backg').eq($(this).index()).addClass('tab-backg');
+    $('.wrapper .tab').click(function () {
+        $('.wrapper .tab').removeClass('active').eq($(this).index()).addClass('active');
+        $('.wrapper .tab').removeClass('tab-backg').eq($(this).index()).addClass('tab-backg');
+        $('.tab_item').hide().eq($(this).index()).fadeIn();
+    }).eq(0).addClass('active');
 
 // ================================================================
 $('.all').mouseenter(function(){
